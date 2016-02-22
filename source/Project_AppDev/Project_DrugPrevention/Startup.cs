56fc81bec3b47d9002709cde88192195c8c6826e
Kevin Strijbos <strijboskevin@hotmail.com>
2016-02-22 21:56:22 +0100

    Create a project with packages.

---
+++ source/Project_AppDev/Project_DrugPrevention/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_DrugPrevention.Startup))]
+namespace Project_DrugPrevention
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
