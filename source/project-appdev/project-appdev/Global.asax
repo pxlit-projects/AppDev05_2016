b44c1a69335dba604c02ed892c65d4552a883f66
Kevin Strijbos <strijboskevin@hotmail.com>
2016-03-18 10:06:17 +0100

    add project

---
+++ source/project-appdev/project-appdev/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="project_appdev.MvcApplication" Language="C#" %>
---
+++ source/project-appdev/project-appdev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(project_appdev.Startup))]
+namespace project_appdev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
