a68f3bd1e0875a955a7eab4d5668e074c378f20a
Kevin Strijbos <strijboskevin@hotmail.com>
2016-04-20 13:32:14 +0200

    added guidelines

--- source/webapp-stufv/webapp-stufv/Models/Guideline.cs
+++ source/webapp-stufv/webapp-stufv/Models/Guideline.cs
@@ -6,6 +6,7 @@ using System.Web;
 namespace webapp_stufv.Models {
     public class Guideline {
         public int Id { get; set; }
+        public string Short { get; set; }
         public string Content { get; set; }
     }
 }
