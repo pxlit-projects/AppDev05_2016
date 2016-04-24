307e7459c3f1c778ac2017e4ca155661a78852a8
Sacha Reyskens <11400982@student.pxl.be>
2016-04-24 16:47:12 +0200

    Update passenger.cs

--- source/webapp-stufv/webapp-stufv/Models/Passenger.cs
+++ source/webapp-stufv/webapp-stufv/Models/Passenger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace webapp_stufv.Models
     [Table("Passenger")]
     public class Passenger
     {
+        [Key]
+        public int Id { get; set; }
         [ForeignKey("User")]
         public int UserId { get; set; }
         [ForeignKey("DesDriver")]
