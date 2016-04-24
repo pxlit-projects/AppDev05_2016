967ff47cc71e3b48f01ffd13cee5a3f701c69f74
BramVV <bram.vanvleymen@student.pxl.be>
2016-04-24 15:08:36 +0200

    Become bob & remove bob

--- source/webapp-stufv/webapp-stufv/Models/DesDriver.cs
+++ source/webapp-stufv/webapp-stufv/Models/DesDriver.cs
@@ -30,5 +30,75 @@ namespace webapp_stufv.Models {
 
         [Required]
         public Boolean Active { get; set; }
+
+        public static void unSetDES(int userId, int eventId) {
+            if (IsDES(userId, eventId))
+            {
+                using (var context = new STUFVModelContext())
+                {
+                    var drivers = context.DesDrivers.FirstOrDefault(c => c.UserId == userId && c.EventId == eventId);
+                    drivers.Active = false;
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public static void SetDES(int userId, int eventId, int places) {
+            if (WasDES(userId, eventId))
+            {
+                //already in db active == false
+                using (var context = new STUFVModelContext())
+                {
+                    var driver = context.DesDrivers.FirstOrDefault(c => c.UserId == userId && c.EventId == eventId);
+                    driver.Active = true;
+                    driver.NrOfPlaces = places;
+                    context.SaveChanges();
+                }
+
+            }
+            else {
+                var driver = new DesDriver{ EventId = eventId, UserId = userId, Active = true, NrOfPlaces = places, NrOfFilled = 0, Flagged = false};
+                using (var context = new STUFVModelContext())
+                {
+                    context.DesDrivers.Add(driver);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public static List<DesDriver> GetAllDrivers() {
+            using (var context = new STUFVModelContext())
+            {
+                List<DesDriver> drivers = new List<DesDriver>();
+                drivers = context.DesDrivers.ToList();
+                return drivers;
+            }
+        }
+
+        public static bool IsDES(int userId, int eventId) {
+            List<DesDriver> drivers = GetAllDrivers();
+                int x;
+                for (x = 0; x < drivers.Count(); x++)
+                {
+                    if (drivers.ElementAt(x).UserId.Equals(userId) && drivers.ElementAt(x).EventId.Equals(eventId) && drivers.ElementAt(x).Active.Equals(true))
+                    {
+                        return true;
+                    }
+            }
+                return false;
+        }
+        private static bool WasDES(int userId, int eventId)
+        {
+            List<DesDriver> drivers = GetAllDrivers();
+            int x;
+            for (x = 0; x < drivers.Count(); x++)
+            {
+                if (drivers.ElementAt(x).UserId.Equals(userId) && drivers.ElementAt(x).EventId.Equals(eventId) && drivers.ElementAt(x).Active.Equals(false))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
