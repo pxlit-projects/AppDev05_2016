c00dad25b50be2dd6df184e14cbb46ddaf0d4b1d
JoranClaessens <joran.claessens@student.pxl.be>
2016-04-28 18:54:27 +0200

    classes en context toegevoegd aan desktopapp

---
+++ source/desktopapp-stufv/STUFV.DomainClasses/Emergency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webapp_stufv.Models {
+    public class Emergency {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TelNr { get; set; }
+        public Boolean Active { get; set; }
+
+        // Methods
+        public static List<Emergency> getAllEmergencies() {
+            using ( var context = new STUFVModelContext ( ) ) {
+                return context.Emergencies.ToList ( );
+            }
+        }
+    }
+}
