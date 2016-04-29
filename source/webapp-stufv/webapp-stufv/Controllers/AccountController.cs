c6483beb82f34209d0d45cc5a93c93289c6017fc
Kevin Strijbos <strijboskevin@hotmail.com>
2016-04-29 14:55:42 +0200

    deployed to azure

--- source/webapp-stufv/webapp-stufv/Controllers/AccountController.cs
+++ source/webapp-stufv/webapp-stufv/Controllers/AccountController.cs
@@ -19,96 +19,99 @@ namespace webapp_stufv.Controllers {
             return View ( );
         }
         public ActionResult Process ( ) {
-            var email = Request.Form[ "Email" ].ToLower();
+            var email = Request.Form[ "Email" ].ToLower ( );
             String password = Request.Form[ "Password" ];
-            if (Models.User.Exist(email))
-            {
+            if ( Models.User.Exist ( email ) ) {
                 int userID;
-                String encPass = MD5Encrypt(password, Models.User.GetSalt(email));
-                if (webapp_stufv.Models.User.Login(email, encPass, out userID))
-                {
-                    Session["email"] = email;
-                    Session["userId"] = userID;
-                    Session["organisation"] = Models.Organisation.HasOrganisation(userID);
+                String encPass = MD5Encrypt ( password, Models.User.GetSalt ( email ) );
+                if ( webapp_stufv.Models.User.Login ( email, encPass, out userID ) ) {
+                    Session[ "email" ] = email;
+                    Session[ "userId" ] = userID;
+                    Session[ "organisation" ] = Models.Organisation.HasOrganisation ( userID );
                     ViewBag.Title = "Succes";
-                    return View();
-                }
-                else {
+                    return View ( );
+                } else {
                     ViewBag.Title = "Login mislukt";
-                    Session["email"] = "";
-                    Session["userId"] = "";
-                    Session["organisation"] = "";
-                    return View();
+                    Session[ "email" ] = "";
+                    Session[ "userId" ] = "";
+                    Session[ "organisation" ] = "";
+                    return View ( );
                 }
-            }
-            else {
+            } else {
                 ViewBag.Title = "Deze gebruiker bestaat nog niet";
-                return View();
+                return View ( );
             }
         }
         public ActionResult Logout ( ) {
             Session[ "Email" ] = "";
-            Session["userId"] = "";
-            Session["organisation"] = "";
+            Session[ "userId" ] = "";
+            Session[ "organisation" ] = "";
             ViewBag.Title = "Logout gelukt";
             return View ( );
         }
 
         public ActionResult CreateAccount ( ) {
             String email = Request.Form[ "Email" ];
-            if (Models.User.Exist(email))
-            {
+            if ( Models.User.Exist ( email ) ) {
                 ViewBag.Title = "Registratie mislukt.";
                 ViewBag.Comment = "Dit email adress is al in gebruik";
-            }
-            else {
-                CreateUser(email);
+            } else {
+                CreateUser ( email );
                 ViewBag.Title = "Registratie";
                 ViewBag.Comment = "Registratie Gelukt";
             }
-            return View ();
+            return View ( );
         }
-        private void CreateUser(String email) {
-            String firstName = Request.Form["FirstName"];
-            String lastName = Request.Form["LastName"];
-            String password = Request.Form["PassWord"];
-            DateTime birthDate = DateTime.Parse(Request.Form["BirthDate"]);
-            String birthPlace = Request.Form["BirthPlace"];
-            Char sex = Char.Parse(Request.Form["Sex"]);
-            String zipCode = Request.Form["ZipCode"];
-            String telNr = Request.Form["TelNr"];
-            String mobileNr = Request.Form["MobileNr"];
-            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-            String salt = GenerateRandomSalt(rng, 16);
-            String encPass = MD5Encrypt(password, salt);
-            var user = new User { FirstName = firstName, LastName = lastName, PassWord = encPass, BirthDate = birthDate, BirthPlace = birthPlace,
-                ZipCode = zipCode, Sex = sex, TelNr = telNr, MobileNr = mobileNr, Salt = salt, Active = true, RoleID = 1, Email = email.ToLower() };
-           using (var context = new STUFVModelContext())
-          {
-             context.Users.Add(user);
-            context.SaveChanges();
-          }
+        private void CreateUser ( String email ) {
+            String firstName = Request.Form[ "FirstName" ];
+            String lastName = Request.Form[ "LastName" ];
+            String password = Request.Form[ "PassWord" ];
+            DateTime birthDate = DateTime.Parse ( Request.Form[ "BirthDate" ] );
+            String birthPlace = Request.Form[ "BirthPlace" ];
+            Char sex = Char.Parse ( Request.Form[ "Sex" ] );
+            String zipCode = Request.Form[ "ZipCode" ];
+            String telNr = Request.Form[ "TelNr" ];
+            String mobileNr = Request.Form[ "MobileNr" ];
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider ( );
+            String salt = GenerateRandomSalt ( rng, 16 );
+            String encPass = MD5Encrypt ( password, salt );
+            var user = new User {
+                FirstName = firstName,
+                LastName = lastName,
+                PassWord = encPass,
+                BirthDate = birthDate,
+                BirthPlace = birthPlace,
+                ZipCode = zipCode,
+                Sex = sex,
+                TelNr = telNr,
+                MobileNr = mobileNr,
+                Salt = salt,
+                Active = true,
+                RoleID = 1,
+                Email = email.ToLower ( )
+            };
+            using ( var context = new STUFVModelContext ( ) ) {
+                context.Users.Add ( user );
+                context.SaveChanges ( );
+            }
         }
-        private string GenerateRandomSalt(RNGCryptoServiceProvider rng, int size)
-        {
-            var bytes = new Byte[size];
-            rng.GetBytes(bytes);
-            return Convert.ToBase64String(bytes);
+        private string GenerateRandomSalt ( RNGCryptoServiceProvider rng, int size ) {
+            var bytes = new Byte[ size ];
+            rng.GetBytes ( bytes );
+            return Convert.ToBase64String ( bytes );
         }
-        private static string MD5Encrypt(string password, string salt)
-        {
-            MD5 md5 = new MD5CryptoServiceProvider();
-            
-            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(salt + password));
+        private static string MD5Encrypt ( string password, string salt ) {
+            MD5 md5 = new MD5CryptoServiceProvider ( );
+
+            md5.ComputeHash ( ASCIIEncoding.ASCII.GetBytes ( salt + password ) );
             byte[] result = md5.Hash;
 
-            StringBuilder strBuilder = new StringBuilder();
-            for (int i = 0; i < result.Length; i++)
-            {
-                strBuilder.Append(result[i].ToString("x2"));
+            StringBuilder strBuilder = new StringBuilder ( );
+            for ( int i = 0 ; i < result.Length ; i++ ) {
+                strBuilder.Append ( result[ i ].ToString ( "x2" ) );
             }
 
-            return strBuilder.ToString();
+            return strBuilder.ToString ( );
         }
     }
 }
