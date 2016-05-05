eefc11e9ab251002119362af5818e8bc10048bbd
JoranClaessens <joran.claessens@student.pxl.be>
2016-05-05 23:11:53 +0200

    beheer artikel bijna compleet

--- source/desktopapp-stufv/STUFV/ManageArticlePage.xaml.cs
+++ source/desktopapp-stufv/STUFV/ManageArticlePage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,11 +23,25 @@ namespace STUFV
     public partial class ManageArticlePage : Page
     {
         HomeWindow scherm = (HomeWindow)Application.Current.MainWindow;
+        HttpClient client = new HttpClient();
 
         public ManageArticlePage()
         {
             InitializeComponent();
+
+            client.BaseAddress = new Uri("http://webapp-stufv20160429025210.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            List<string> filterItems = new List<string> { "Id", "Titel", "AuteurId", "Datum aangemaakt", "Datum vanaf" };
+
+            filterBox.ItemsSource = filterItems;
+            filterBox.SelectedIndex = 1;
+
             menuBox.SelectionChanged += MenuBox_SelectionChanged;
+            filterBox.SelectionChanged += FilterBox_SelectionChanged;
+            searchDatePicker.SelectedDateChanged += SearchDatePicker_SelectedDateChanged;
+            LoadArticles();
         }
 
         private void MenuBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -62,6 +78,133 @@ namespace STUFV
                     scherm.displayFrame.Source = new Uri("LogoutPage.xaml", UriKind.Relative);
                     break;
             }
+        }
+
+        private void FilterBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (filterBox.Text == "Datum aangemaakt" || filterBox.Text == "Datum vanaf")
+            {
+                searchDatePicker.Visibility = Visibility.Visible;
+                searchTextBox.IsReadOnly = true;
+            }
+            else
+            {
+                searchDatePicker.Visibility = Visibility.Hidden;
+                searchTextBox.IsReadOnly = false;
+            }
+        }
+
+
+        private async void SearchTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (searchTextBox.Text != "")
+            {
+                IEnumerable<Article> allArticles = await GetArticles();
+
+                string filter = filterBox.Text;
+
+                List<Article> selectArticles = new List<Article>();
+                foreach (Article article in allArticles)
+                {
+                    switch (filter)
+                    {
+                        case "Id":
+                            int id = 0;
+                            if (int.TryParse(searchTextBox.Text, out id))
+                            {
+                                id = Convert.ToInt32(searchTextBox.Text);
+                            }
+                            if (article.Id == id) { selectArticles.Add(article); }
+                            break;
+                        case "Titel":
+                            if (article.Title.Contains(searchTextBox.Text)) { selectArticles.Add(article); }
+                            break;
+                        case "AuteurId":
+                            int auteurId = 0;
+                            if (int.TryParse(searchTextBox.Text, out auteurId))
+                            {
+                                auteurId = Convert.ToInt32(searchTextBox.Text);
+                            }
+                            if (article.UserId == auteurId) { selectArticles.Add(article); }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (selectArticles == null)
+                {
+                    messageLabel.Content = "Geen resultaten";
+                }
+                else
+                {
+                    messageLabel.Content = "Er zijn " + selectArticles.Count + " resultaten gevonden!";
+                    articlesDataGrid.ItemsSource = selectArticles;
+                }
+            }
+            else
+            {
+                messageLabel.Content = "";
+                LoadArticles();
+            }
+        }
+
+
+        private async void SearchDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (searchDatePicker.SelectedDate != null)
+            {
+                IEnumerable<Article> allArticles = await GetArticles();
+
+                string filter = filterBox.Text;
+
+                List<Article> selectArticles = new List<Article>();
+                foreach (Article article in allArticles)
+                {
+                    switch (filter)
+                    {
+                        case "Datum aangemaakt":
+                            if (article.DateTime == searchDatePicker.SelectedDate) { selectArticles.Add(article); }
+                            break;
+                        case "Datum vanaf":
+                            if (article.DateTime >= searchDatePicker.SelectedDate) { selectArticles.Add(article); }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                if (selectArticles == null)
+                {
+                    messageLabel.Content = "Geen resultaten";
+                }
+                else
+                {
+                    messageLabel.Content = "Er zijn " + selectArticles.Count + " resultaten gevonden!";
+                    articlesDataGrid.ItemsSource = selectArticles;
+                }
+            }
+            else
+            {
+                messageLabel.Content = "";
+                LoadArticles();
+            }
+        }
+
+        private async void LoadArticles()
+        {
+            articlesDataGrid.ItemsSource = await GetArticles();
+        }
+
+        public async Task<IEnumerable<Article>> GetArticles()
+        {
+            var userUrl = "/api/article";
+            HttpResponseMessage response = await client.GetAsync(userUrl);
+            IEnumerable<Article> articles = null;
+
+            if (response.IsSuccessStatusCode)
+            {
+                articles = await response.Content.ReadAsAsync<IEnumerable<Article>>();
+            }
+            return articles;
         }
     }
 }
