187c5f99f665eed0a668a4c028cfbbf42681bd12
BramVV <bram.vanvleymen@student.pxl.be>
2016-05-07 15:36:28 +0200

    Profile Controller

--- source/webapp-stufv/webapp-stufv/Controllers/ProfileController.cs
+++ source/webapp-stufv/webapp-stufv/Controllers/ProfileController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using webapp_stufv.Models;
+using webapp_stufv.Repository;
 
 namespace webapp_stufv.Controllers
 {
     public class ProfileController : Controller
     {
+        IUserRepository iuser = new UserRepository();
         public ActionResult Index()
         {
-            return View();
+            List<User> allUsers = iuser.GetAllUsers();
+            return View(allUsers);
+        }
+        public ActionResult Users(int id)
+        {
+            List<User> allUsers = iuser.GetAllUsers();
+            User user = allUsers.Single(r => r.Id == id);
+            return View(user);
+        }
+        public ActionResult MyProfile()
+        {
+            List<User> allUsers = iuser.GetAllUsers();
+            User user = allUsers.Single(r => r.Id == (int)Session["userId"]);
+            return View(user);
         }
     }
 }
