769f8c947ba5faf093e0f9b8ee259b40a54d02f0
BramVV <bram.vanvleymen@student.pxl.be>
2016-05-12 01:33:06 +0200

    tip table

---
+++ source/webapp-stufv/webapp-stufv/Models/Tip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapp_stufv.Models
+{
+    [Table("Tip")]
+    public class Tip
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TipText { get; set; }
+        [Required]
+        public Boolean Active { get; set; }
+
+
+    }
+}
