3aeef0e620cd8560af81283ab9ce8d1a693f3254
BramVV <bram.vanvleymen@student.pxl.be>
2016-05-11 22:14:20 +0200

    settings controller

---
+++ source/webapp-stufv/webapp-stufv/Controllers/SettingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webapp_stufv.Models;
+using webapp_stufv.Repository;
+
+namespace webapp_stufv.Controllers
+{
+    public class SettingsController : Controller
+    {
+        IUserRepository iuser = new UserRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MyProfile()
+        {
+            List<User> allUsers = iuser.GetAllUsers();
+            User user = allUsers.Single(r => r.Id == (int)Session["userId"]);
+            return View(user);
+        }
+    }
+}
