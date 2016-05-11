7bda12e7d0b6835af1be076e9c812e5e94f6fa68
BramVV <bram.vanvleymen@student.pxl.be>
2016-05-11 22:30:43 +0200

    Change Profile picture

--- source/webapp-stufv/webapp-stufv/Controllers/SettingsController.cs
+++ source/webapp-stufv/webapp-stufv/Controllers/SettingsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,13 +14,29 @@ namespace webapp_stufv.Controllers
         IUserRepository iuser = new UserRepository();
         public ActionResult Index()
         {
-            return View();
-        }
-        public ActionResult MyProfile()
-        {
             List<User> allUsers = iuser.GetAllUsers();
             User user = allUsers.Single(r => r.Id == (int)Session["userId"]);
             return View(user);
+        }
+        public ActionResult ProfileImgUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath(@"..\Content\img\ProfilePictures\"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+            }
+            //Change user profile picture
+            using (var context = new STUFVModelContext())
+            {
+                int userId = (int)Session["userId"];
+                var user = context.Users.FirstOrDefault(c => c.Id == userId);
+                user.ProfilePicture = file.FileName;
+                context.SaveChanges();
+            }
+            return RedirectToAction("actionname", "controller name");
         }
     }
 }
