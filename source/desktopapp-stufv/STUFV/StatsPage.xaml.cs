0d62e7fafa4b1ceb8f90d9e7b079d1fab39474a5
Sacha Reyskens <11400982@student.pxl.be>
2016-05-18 01:22:17 +0200

    Testing phase statspage

--- source/desktopapp-stufv/STUFV/StatsPage.xaml.cs
+++ source/desktopapp-stufv/STUFV/StatsPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,11 +23,16 @@ namespace STUFV
     public partial class StatsPage : Page
     {
         HomeWindow scherm = (HomeWindow)Application.Current.MainWindow;
+        HttpClient client = new HttpClient();
 
         public StatsPage()
         {
             InitializeComponent();
             menuBox.SelectionChanged += MenuBox_SelectionChanged;
+
+            client.BaseAddress = new Uri("http://webapp-stufv20160511012914.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         private void MenuBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -71,6 +78,126 @@ namespace STUFV
                     scherm.displayFrame.Source = new Uri("LogoutPage.xaml", UriKind.Relative);
                     break;
             }
+        }
+
+        public async void loadAllStats()
+        {
+            IEnumerable < Review > reviews = await GetReviews();
+            IEnumerable < User > users = await GetUsers();
+            IEnumerable < Organisation > organisations = await GetOrganisations();
+            IEnumerable<Event> events = await GetEvents();
+            int reviewCount = reviews.ToList().Count();
+            int userCount = users.ToList().Count();
+            int organisationCount = organisations.ToList().Count();
+            int eventCount = events.ToList().Count();
+            userLabel.Content = userCount.ToString();
+            organisationLabel.Content = organisationCount.ToString();
+            reviewLabel.Content = reviewCount.ToString();
+            eventLabel.Content = eventCount.ToString();
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            IEnumerable<User> users = null;
+            try
+            {
+                var userUrl = "/api/user";
+                HttpResponseMessage response = await client.GetAsync(userUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    users = await response.Content.ReadAsAsync<IEnumerable<User>>();
+                }
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Verbinding met de server verbroken. Probeer later opnieuw. U zal worden doorverwezen naar het loginscherm.",
+                    "Serverfout", MessageBoxButton.OK, MessageBoxImage.Error);
+                LoginWindow window = new LoginWindow();
+                window.Show();
+                scherm.Close();
+            }
+            return users;
+        }
+
+        public async Task<IEnumerable<Review>> GetReviews()
+        {
+            IEnumerable<Review> reviews = null;
+            try
+            {
+                var reviewUrl = "/api/review";
+                HttpResponseMessage response = await client.GetAsync(reviewUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    reviews = await response.Content.ReadAsAsync<IEnumerable<Review>>();
+                }
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Verbinding met de server verbroken. Probeer later opnieuw. U zal worden doorverwezen naar het loginscherm.",
+                    "Serverfout", MessageBoxButton.OK, MessageBoxImage.Error);
+                LoginWindow window = new LoginWindow();
+                window.Show();
+                scherm.Close();
+            }
+            return reviews;
+        }
+
+        public async Task<IEnumerable<Organisation>> GetOrganisations()
+        {
+            IEnumerable<Organisation> organisations = null;
+            try
+            {
+                var organisationUrl = "/api/organisations";
+                HttpResponseMessage response = await client.GetAsync(organisationUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    organisations = await response.Content.ReadAsAsync<IEnumerable<Organisation>>();
+                }
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Verbinding met de server verbroken. Probeer later opnieuw. U zal worden doorverwezen naar het loginscherm.",
+                    "Serverfout", MessageBoxButton.OK, MessageBoxImage.Error);
+                LoginWindow window = new LoginWindow();
+                window.Show();
+                scherm.Close();
+            }
+            return organisations;
+        }
+
+        private async Task<IEnumerable<Event>> GetEvents()
+        {
+            IEnumerable<Event> events = null;
+            List<Event> relatedEvents = new List<Event>();
+            try
+            {
+                var eventUrl = "/api/event";
+                HttpResponseMessage response = await client.GetAsync(eventUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    events = await response.Content.ReadAsAsync<IEnumerable<Event>>();
+                    foreach (Event orgEvent in events)
+                    {
+                        if (orgEvent.Handled == true)
+                        {
+                            relatedEvents.Add(orgEvent);
+                        }
+                    }
+                }
+            }
+            catch (HttpRequestException)
+            {
+                MessageBox.Show("Verbinding met de server verbroken. Probeer later opnieuw. U zal worden doorverwezen naar het loginscherm.",
+                    "Serverfout", MessageBoxButton.OK, MessageBoxImage.Error);
+                LoginWindow window = new LoginWindow();
+                window.Show();
+                scherm.Close();
+            }
+            return relatedEvents;
         }
     }
 }
