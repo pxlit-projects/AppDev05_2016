d4f447c09da6a28e3240bc611d8d83fcf7a1f2d8
fred1993 <fredvw_d12@hotmail.com>
2016-05-18 22:31:34 +0200

    Adding TipRepository + Making lay-out Tipbox

---
+++ source/webapp-stufv/webapp-stufv/Repository/TipRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using webapp_stufv.Models;
+
+namespace webapp_stufv.Repository
+{
+    public class TipRepository : ITipRepository
+    {
+        public List<Tip> getAllTips()
+        {
+            using (var context = new STUFVModelContext())
+            {
+                List<Tip> tips = new List<Tip>();
+                tips = context.Tips.ToList();
+                return tips;
+            }
+        }
+    }
+}
