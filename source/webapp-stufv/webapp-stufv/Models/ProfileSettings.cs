8aeabf29450846640bce3f39d2b344c423bf5286
BramVV <bram.vanvleymen@student.pxl.be>
2016-05-19 14:43:32 +0200

    profile settings layout + model

---
+++ source/webapp-stufv/webapp-stufv/Models/ProfileSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace webapp_stufv.Models
+{
+    [Table("ProfileSettings")]
+    public class ProfileSettings
+    {
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        [Required]
+        public Boolean Email { get; set; }
+        [Required]
+        public Boolean FirstName { get; set; }
+        [Required]
+        public Boolean LastName { get; set; }
+        [Required]
+        public Boolean BirthDate { get; set; }
+        [Required]
+        public Boolean Street { get; set; }
+        [Required]
+        public Boolean ZipCode { get; set; }
+        [Required]
+        public Boolean MobileNr { get; set; }
+        [Required]
+        public Boolean TelNr { get; set; }
+    }
+}
