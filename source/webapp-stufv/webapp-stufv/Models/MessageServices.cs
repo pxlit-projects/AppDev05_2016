f619eb9d711b0423614292c3d452bf94dd81a0c7
fred1993 <fredvw_d12@hotmail.com>
2016-05-19 11:20:32 +0200

    Aanmaak emailModel

---
+++ source/webapp-stufv/webapp-stufv/Models/MessageServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace webapp_stufv.Models
+{
+    public class MessageServices
+    {
+
+    }
+}
