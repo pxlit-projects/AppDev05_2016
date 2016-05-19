621cbde9d0b8e15a08c3cf31fef72169c358113e
fred1993 <fredvw_d12@hotmail.com>
2016-05-19 18:03:15 +0200

    add partners + fixed controller and view

--- source/webapp-stufv/webapp-stufv/Controllers/PartnerController.cs
+++ source/webapp-stufv/webapp-stufv/Controllers/PartnerController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using webapp_stufv.Models;
+using webapp_stufv.Repository;
 
 namespace webapp_stufv.Controllers
 {
     public class PartnerController : Controller
     {
-        // GET: Partner
+        private IPartnerRepository ipartner = new PartnerRepository();
+
         public ActionResult Index()
         {
+            List<Partner> partner = ipartner.GetAllPartners();
+            ViewBag.Partner = partner;
+
             return View();
         }
+            
+
     }
 }
